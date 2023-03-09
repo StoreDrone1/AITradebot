9c9ff12921236af791ea1c3038ec3f41f20f4f65
StoreDrone1 <61709896+StoreDrone1@users.noreply.github.com>
2023-03-08 23:17:48 -0500

    Add files via upload
    Update the API key and secret key each time you navigate away from the webpage

---
+++ AITRADEBOT.py
@@ -0,0 +1,136 @@
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+import alpaca_trade_api as trade_api
+from alpaca_trade_api.rest import TimeFrame
+#from alpaca_trade_api import Alpaca
+import time as t
+import schedule
+from TrainFinancialModel import train_model
+
+#ticker symbol for apple, CHANGE THIS!
+symbol = "KSCP"
+# Connect to the Alpaca API
+#API KEY ID , then SECRET KEY
+#you will have to input your api key / secret code respectively here
+api = trade_api.REST('AK1WLH5VISIWR62IZQXC', 'vvq4JGnNtCDgZ4SQm2MUM9JaghKjiv91BTufKnFW')
+
+# Download historical stock prices from Yahoo Finance
+ticker = "KSCP"
+df = yf.download(ticker, start="2022-02-11")
+
+# Preprocess the data
+scaler = MinMaxScaler()
+df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+#print(df.tail(5))
+# Split the data into training and testing sets
+train_data = df.iloc[:int(df.shape[0]*0.8), :]
+test_data = df.iloc[int(df.shape[0]*0.8):, :]
+#print("Test data = {} Train data = {}".format(test_data, train_data))
+# Define the LSTM model
+model = Sequential()
+model.add(LSTM(100, input_shape=(train_data.shape[1], 1)))
+model.add(Dense(5))
+model.compile(loss='mean_squared_error', optimizer='adam', metrics='accuracy')
+
+# Reshape the data for input into the LSTM model
+X_train = np.array(train_data).reshape((train_data.shape[0], train_data.shape[1], 1))
+
+# Train the LSTM model
+try:
+    model.fit(X_train, train_data['Close'], epochs=20, batch_size=64, verbose=2)
+except Exception as e:
+    print("Unable to fit model - Error {}".format(e))
+
+model, X_test = train_model()
+print("Finished training, pending trades...")
+
+model.save("Trained_KSCP.h5")
+
+if type(symbol) == list and len(symbol) == 1:
+        symbol = symbol[0]
+
+'''def process_data(data, look_back=1):
+    X, Y = [], []
+    for i in range(len(data) - look_back):
+        a = data[i:(i + look_back), 0]
+        X.append(a)
+        Y.append(data[i + look_back, 0])
+    return np.array(X), np.array(Y)'''
+
+def trade():
+    print("Thinking about trading")
+    model, X_test = train_model()
+    
+    #get our balance before buying
+    account = api.get_account()
+    balance = float(account.cash)    
+    # get the latest data and update the training data
+    
+    # get the predictions from the updated model
+    try:
+        predictions = model.predict(X_test)
+    except Exception as e:
+        print("Unable to generate predictions - Error {}".format(e))
+
+    pred = predictions[0]
+    #get stock price
+    barset = api.get_bars(symbol, TimeFrame.Day, limit=1)
+    #not sure why we're losing this here, but CHECK TO FUCKING SEE
+    if barset:
+        symbol_bars = barset[0]
+        stock_price = symbol_bars.c
+    else:
+        last_trade = api.get_latest_trade(symbol).price
+        stock_price = float(last_trade)
+        #print("Last trade price = {}".format(stock_price))
+    #shares = int(amount / stock_price)
+    #calculate number of shares that can be bought with the amount
+    # assume that we want to buy if the prediction is positive and sell if the prediction is negative
+    #print("Predicted price of {} = {}".format(symbol, predictions[0]))
+    if predictions[0] >= 0.07:
+        # place a buy order
+        if balance > stock_price:
+            api.submit_order(
+            symbol=symbol,
+            qty=1,
+            side='buy',
+            type='market',
+            time_in_force='gtc'
+        )
+        else:
+            print("Insufficient funds for buy")
+    elif predictions[0] <= 0.07:
+        # place a sell order
+        api.submit_order(
+            symbol=symbol,
+            qty=1,
+            side='sell',
+            type='market',
+            time_in_force='gtc'
+        )
+    with open("Predictions_Today.txt", 'a') as preds:
+        preds.writelines(str(predictions[0]))
+    print("Predictions[0]: ", predictions[0])
+    #clear the barset.  Hopefully this will eliminate the error
+    #that the barset index is out of bounds
+    barset.clear()    
+
+# schedule the trade function to run at certain intervals every day
+#schedule.every().day.at("19:00").do(trade)
+schedule.every().day.at("09:00").do(trade)
+schedule.every().day.at("10:00").do(trade)
+schedule.every().day.at("11:00").do(trade)
+schedule.every().day.at("12:01").do(trade)
+schedule.every().day.at("13:00").do(trade)
+schedule.every().day.at("14:00").do(trade)
+schedule.every().day.at("15:00").do(trade)
+schedule.every().day.at("16:00").do(trade)
+
+while True: 
+    schedule.run_pending()
+    t.sleep(1)
+   
---
+++ TrainFinancialModel.py
@@ -0,0 +1,33 @@
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+
+def train_model():
+    # Download historical stock prices from Yahoo Finance
+    ticker = "HOOD"
+    df = yf.download(ticker, start="2022-02-11")
+
+    # Preprocess the data
+    scaler = MinMaxScaler()
+    df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+
+    # Split the data into training and testing sets
+    train_data = df.iloc[:int(df.shape[0]*0.8), :]
+    test_data = df.iloc[int(df.shape[0]*0.8):, :]
+
+    # Define the LSTM model
+    model = Sequential()
+    model.add(LSTM(50, input_shape=(train_data.shape[1], 1)))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+
+    # Reshape the data for input into the LSTM model
+    X_train = np.array(train_data).reshape((train_data.shape[0], train_data.shape[1], 1))
+    X_test = np.array(test_data).reshape((test_data.shape[0], test_data.shape[1], 1))
+
+    # Train the LSTM model
+    model.fit(X_train, train_data[['Open', 'Close']], epochs=100, batch_size=1, verbose=2)
+    return model, X_test
