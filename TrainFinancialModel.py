92df9d851added7dd3f08b0bb46cda7c5bad0094
StoreDrone1 <61709896+StoreDrone1@users.noreply.github.com>
2023-03-31 13:58:28 -0400

    Update TrainFinancialModel.py

--- TrainFinancialModel.py
+++ TrainFinancialModel.py
@@ -4,10 +4,13 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
+#cant double call the optimizer
+#from tensorflow.keras.optimizers import Adam
+
 
 def train_model():
     # Download historical stock prices from Yahoo Finance
-    ticker = "HOOD"
+    ticker = "KSCP"
     df = yf.download(ticker, start="2022-02-11")
 
     # Preprocess the data
@@ -20,14 +23,14 @@ def train_model():
 
     # Define the LSTM model
     model = Sequential()
-    model.add(LSTM(50, input_shape=(train_data.shape[1], 1)))
-    model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.add(LSTM(150, input_shape=(train_data.shape[1], 1)))
+    model.add(Dense(2))
+    model.compile(loss='mean_squared_error', optimizer="adam")
 
     # Reshape the data for input into the LSTM model
     X_train = np.array(train_data).reshape((train_data.shape[0], train_data.shape[1], 1))
     X_test = np.array(test_data).reshape((test_data.shape[0], test_data.shape[1], 1))
 
     # Train the LSTM model
-    model.fit(X_train, train_data[['Open', 'Close']], epochs=100, batch_size=1, verbose=2)
+    model.fit(X_train, train_data['Close'], epochs=32, batch_size=32, verbose=2)
     return model, X_test
